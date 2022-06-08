d8e8e2ca5e1d97b9961f588655b04ccf4696d7ae
DELL\necoj <nemanja.jovic195@gmail.com>
2022-06-08 23:59:44 +0200

    Added dummy data

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { FiSettings } from "react-icons/fi";
+import { TooltipComponent } from "@syncfusion/ej2-react-popups";
 
 import "./App.css";
 
