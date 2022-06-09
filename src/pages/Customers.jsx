10985f520dfb76acf87fe7a145a52fb25d10bedc
DELL\necoj <nemanja.jovic195@gmail.com>
2022-06-09 20:52:00 +0200

    Added customers and Employees page

--- src/pages/Customers.jsx
+++ src/pages/Customers.jsx
@@ -1,9 +1,44 @@
-import React from 'react'
+import React from "react";
+import {
+  GridComponent,
+  ColumnsDirective,
+  ColumnDirective,
+  Page,
+  Selection,
+  Inject,
+  Edit,
+  Toolbar,
+  Sort,
+  Filter,
+} from "@syncfusion/ej2-react-grids";
+
+import { customersData, customersGrid } from "../data/dummy";
+import { Header } from "../components";
 
 const Customers = () => {
   return (
-    <div>Customers</div>
-  )
-}
+    <div className="m-2 md:m-10 p-2 md:p-10 bg-white rounded-3xl">
+      <Header title="Customers" category="Page" />
+      <GridComponent
+        dataSource={customersData}
+        allowPaging
+        allowSorting
+        width="auto"
+        toolbar={["Delete", "Edit"]}
+        editSettings={{
+          allowDeleting: true,
+          allowEditing: true,
+        }}
+      >
+        <ColumnsDirective>
+          {customersGrid.map((item, id) => (
+            <ColumnDirective key={id} {...item} />
+          ))}
+        </ColumnsDirective>
+        <Inject services={[Page, Toolbar, Sort, Selection, Edit, Filter]} />
+      </GridComponent>
+    </div>
+  );
+};
 
-export default Customers
+export default Customers;
--- src/pages/Employees.jsx
+++ src/pages/Employees.jsx
@@ -1,9 +1,37 @@
-import React from 'react'
+import React from "react";
+import {
+  GridComponent,
+  ColumnsDirective,
+  ColumnDirective,
+  Page,
+  Search,
+  Edit,
+  Inject,
+  Toolbar,
+} from "@syncfusion/ej2-react-grids";
 
-const Emplyees = () => {
+import { employeesData, contextMenuItems, employeesGrid } from "../data/dummy";
+import { Header } from "../components";
+
+const Employees = () => {
   return (
-    <div>Emplyees</div>
-  )
-}
+    <div className="m-2 md:m-10 p-2 md:p-10 bg-white rounded-3xl">
+      <Header title="Employees" category="Page" />
+      <GridComponent
+        dataSource={employeesData}
+        allowPaging
+        toolbar={["Search"]}
+        width="auto"
+      >
+        <ColumnsDirective>
+          {employeesGrid.map((item, id) => (
+            <ColumnDirective key={id} {...item} />
+          ))}
+        </ColumnsDirective>
+        <Inject services={[Page, Search, Toolbar]} />
+      </GridComponent>
+    </div>
+  );
+};
 
-export default Emplyees
+export default Employees;
